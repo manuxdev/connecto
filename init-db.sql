7c3fd2e9af736e8a48869358bc353271d3f062ea
Manuel Pantoja <manuxdev1010@gmail.com>
2024-02-26 13:35:19 -0500

    Update init-db.sql

--- init-db.sql
+++ init-db.sql
@@ -1,11 +1,11 @@
 DROP EXTENSION IF EXISTS "uuid-ossp";
 CREATE EXTENSION "uuid-ossp";
 
--- DROP TABLE IF EXISTS tweets;
--- DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS tweets;
+DROP TABLE IF EXISTS users;
 
--- DROP TABLE IF EXISTS likes;
--- DROP TABLE IF EXISTS followers;
+DROP TABLE IF EXISTS likes;
+DROP TABLE IF EXISTS followers;
 
 --Create Tables
 
@@ -127,53 +127,3 @@ EXECUTE FUNCTION increment_tweet_comments();
 
 
 
-INSERT INTO users (user_handle , email_address , first_name,last_name , phonenumber) 
-VALUES
-('manuxdev', 'manuxdev1010@gmail.com', 'Manuel', 'Pantoja','54559131'),
-('ramiro', 'ramiro1234@gmail.com', 'Ramiro', 'Ramirez','53559112'),
-('jesus', 'jesussuto@gmail.com', 'Jesus', 'Ramos','53559132'),
-('flores', 'flores@gmail.com', 'Flores', 'Perez','53559122'),
-('julito', 'manuelpo@gmail.com', 'Julito', 'Perez','53529122');
-
-
-INSERT INTO users (user_handle , email_address , first_name,last_name , phonenumber) 
-VALUES
-('manuxdev', 'manuxdev1010@gmail.com', 'Manuel', 'Pantoja','54559131'),
-('ramiro', 'ramiro1234@gmail.com', 'Ramiro', 'Ramirez','53559112'),
-('jesus', 'jesussuto@gmail.com', 'Jesus', 'Ramos','53559132'),
-('flores', 'flores@gmail.com', 'Flores', 'Perez','53559122'),
-('julito', 'manuelpo@gmail.com', 'Julito', 'Perez','53529122');
-
-
-INSERT INTO followers (follower_id , following_id)
-VALUES
-('dc4a86d6-31a2-47aa-82d8-b3b5d9aa72d5','37e16e4c-36c7-4ecd-9c66-06f724930c62'),
-('dc4a86d6-31a2-47aa-82d8-b3b5d9aa72d5','37e16e4c-36c7-4ecd-9c66-06f724930c62'),
-('37e16e4c-36c7-4ecd-9c66-06f724930c62','dc4a86d6-31a2-47aa-82d8-b3b5d9aa72d5');
-
-INSERT INTO tweets (user_id, tweet_text)
-VALUES
-
-('dc4a86d6-31a2-47aa-82d8-b3b5d9aa72d5', 'Tweet1✋'),
-('dc4a86d6-31a2-47aa-82d8-b3b5d9aa72d5', 'Tweet2✋'),
-('dc4a86d6-31a2-47aa-82d8-b3b5d9aa72d5', 'Tweet3✋');
-
-INSERT INTO likes (user_id , tweet_id)
-VALUES
-
-('f34ab9e7-51e7-432f-80fc-de526bb300db','1c9bc65c-51b6-4c0f-a653-4e21e60b8d62');
-
-
-INSERT INTO comments (user_id, tweet_id, comment)
-VALUES 
-('dc4a86d6-31a2-47aa-82d8-b3b5d9aa72d5', 'da731190-9d8f-4cd4-bb7b-47d3ab2d3eaa','Comentario en tweet 3 de manuxdev');
-
-
-INSERT INTO bookmarks (user_id , tweet_id)
-VALUES
-('dc4a86d6-31a2-47aa-82d8-b3b5d9aa72d5','1c9bc65c-51b6-4c0f-a653-4e21e60b8d62');
-
-
-INSERT INTO notifications (receiver, type, tweetid )
-VALUES
-('dc4a86d6-31a2-47aa-82d8-b3b5d9aa72d5', 'follow', '697c9876-adc3-4296-9ca5-ddec2d81748c');
